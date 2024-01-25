4cbf24a1d6575dea77e9ed32a367c34a680ae263
millandj <dsm6840@students.uc-bcf.edu.ph>
2024-01-26 06:38:45 +0800

    Add BankAccount class

---
+++ BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        public void MakeDeposit(decimal Amount, DateTime date, string note)
+        {
+
+        }
+        public void MakeWithdrawal(decimal Amount, DateTime date, string note)
+        {
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,9 +2,14 @@
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
             
+
+
+
         }
     }
 }
