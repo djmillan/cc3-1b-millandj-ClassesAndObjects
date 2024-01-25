07c350f37993980ec59ead037f39199332d24827
DevinMillan <millan.devinj@gmail.com>
2024-01-26 06:21:12 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassesAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
