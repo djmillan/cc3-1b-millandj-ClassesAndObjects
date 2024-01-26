cd43cd2ebb56ff79598a63dc6706fc194552dd57
millandj <dsm6840@students.uc-bcf.edu.ph>
2024-01-26 19:14:16 +0800

    Add try catch exception

--- Program.cs
+++ Program.cs
@@ -6,36 +6,48 @@
 
         static void Main(string[] args)
         {
-            //while(true)
-            //{
-            //Console.Write("Enter your name >> ");
-            //string user = Console.ReadLine();
-            //Console.Write("Enter your initial balance >> ");
-            //decimal ibal = Convert.ToDecimal(Console.ReadLine());
-            //Console.Write("Withdraw/Deposit >> ");
-            //string with = Console.ReadLine();
-            BankAccount account = new BankAccount("Joe",1000);
+            while(true)
+            {
+            Console.Write("Enter your name >> ");
+            string user = Console.ReadLine();
+            Console.Write("Enter your initial balance >> ");
+            decimal ibal = Convert.ToDecimal(Console.ReadLine());
+            BankAccount account = new BankAccount(user,ibal);
             Console.WriteLine($"{account.AccountNumber} | {account.Owner} | ${account.Balance}");
-                //if(user.Equals("Withdraw"))
-                //{
-                    //Console.Write("Enter the amount of withdrawal >> ");
-                    //decimal withdrawal = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Withdraw/Deposit >> ");
+                string note;
+            string opt = Console.ReadLine();
+            if (opt.Equals("Withdraw"))
+            {
+                Console.Write("Enter the amount of withdrawal >> ");
+                decimal withdrawal = Convert.ToDecimal(Console.ReadLine());
 
-                    //Console.Write("Input a note >> ");
-                    //string note = Console.ReadLine();
-                    account.MakeWithdrawal(500,DateTime.Now, "Pay rent");
+                Console.Write("Input a note >> ");
+                note = Console.ReadLine();
+                account.MakeWithdrawal(withdrawal, DateTime.Now, note);
 
-                //} else if(user.Equals("Deposit"))
-                //{
-                    //Console.Write("Enter the amount of deposit >> ");
-                    //decimal deposit = Convert.ToDecimal(Console.ReadLine());
+            }
+            else if (user.Equals("Deposit"))
+            {
+                Console.Write("Enter the amount of deposit >> ");
+                decimal deposit = Convert.ToDecimal(Console.ReadLine());
 
-                    //Console.Write("Input a note >> ");
-                    //string notes = Console.ReadLine();
-                    account.MakeDeposit(100, DateTime.Now, "Friend's payment");
-                //}
-
-                /*
+                Console.Write("Input a note >> ");
+                note = Console.ReadLine();
+                account.MakeDeposit(deposit, DateTime.Now, note);
+            }
+            BankAccount invalidAccount;
+            try
+            {
+                invalidAccount = new BankAccount("Invalid", -55);
+            } 
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance.");
+                Console.WriteLine(e.Message);
+                return;
+            }
+            
             Console.Write("Do you want to continue? >> ");
             string option = Console.ReadLine();
             if(option.Equals("yes"))
@@ -50,8 +62,7 @@
                 {
                     break;
                 }
-                */
-            //}
+            }
 
         }
     }
