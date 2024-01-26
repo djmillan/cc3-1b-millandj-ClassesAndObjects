2020781c2a11c49235d50fda9243c83edf1e300f
millandj <dsm6840@students.uc-bcf.edu.ph>
2024-01-26 17:27:04 +0800

    Add AccountNumber feature

--- Program.cs
+++ Program.cs
@@ -6,9 +6,31 @@
 
         static void Main(string[] args)
         {
-            
+            while(true)
+            {
+            Console.Write("Enter your name >> ");
+            string user = Console.ReadLine();
+            Console.Write("Enter your initial balance >> ");
+            decimal ibal = Convert.ToDecimal(Console.ReadLine());
+            BankAccount account = new BankAccount(user,ibal);
 
+            Console.WriteLine($"{account.AccountNumber} | {account.Owner} | ${account.Balance}");
+            Console.Write("Do you want to continue? >> ");
+            string option = Console.ReadLine();
+            if(option.Equals("yes"))
+                {
+                    continue;
+                }
+            else if(option.Equals("no"))
+                {
+                    break;
+                }
+            else
+                {
+                    break;
+                }
 
+            }
 
         }
     }
